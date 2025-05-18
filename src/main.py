26bb48865db8290e4a00358839154e825084d883
Solaiman Hossain <slmnsnto@gmail.com>
2025-05-18 23:28:32 +0600

    feat(message-stat): statistics of the messages
    - Added message statistics functinality
    - Modified the read_file
    - Changed filename read_file to chat_handler
    - Changed classname FileReader to ChatHandler

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-from src import FileReader
+from src import ChatHandler
+
+def message_statistics(ch: ChatHandler):
+    print("==============Count Messages==============")
+    print(f"AI message count: {ch.ai_message_count}")
+    print(f"User message count: {ch.user_message_count}")
+    print(f"Total message count: {ch.total_message_count}")
+
+    print("===============Messages=====================")
+    print(f"User Messages: {ch.user_messages}")
+    print(f"AI Messages: {ch.ai_messages}")
+    print(f"Chat history: {ch.chat_history}")
+
 
 def main():
     path = r'../chats/sql.txt'
-    fr = FileReader()(path)
+    ch = ChatHandler()(path)
 
-    print(fr.chat_history)
-    print(fr.user_messages)
-    print(fr.ai_messages)
+    message_statistics(ch)
+
 
 if __name__ == '__main__':
     main()
